5acfa7f2ee11c5a7da3bd778a3d0226de4d6e6bc
shumkova <a.shumkova@live.com>
2020-02-12 16:38:46 +0700

    устанавливает prop-types и настраивает валидацию

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const Settings = {
-  RENTAL_OFFERS: 312
+  RENTAL_OFFERS_COUNT: 312
 };
+
+const rentalOffers = [
+  `Beautiful & luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+  `Wood and stone place`
+];
 
 ReactDOM.render(
     <App
-      rentalOffers={Settings.RENTAL_OFFERS}
+      rentalOffersCount={Settings.RENTAL_OFFERS_COUNT}
+      rentalOffers={rentalOffers}
     />,
     document.querySelector(`#root`)
 );
